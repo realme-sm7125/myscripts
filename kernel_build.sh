0f53fb22bba45354c393a1619acb18a78d48ceb3
Mayur <ultramayur123@gmail.com>
2022-01-17 12:49:28 +0530

    set indian timezone in VM
    Signed-off-by: Mayur <ultramayur123@gmail.com>

--- kernel_build.sh
+++ kernel_build.sh
@@ -58,6 +58,18 @@ VERBOSE=0
 		export SERVER_URL="${DRONE_SYSTEM_PROTO}://${DRONE_SYSTEM_HOSTNAME}/ayash92/${BASEDIR}/${KBUILD_BUILD_VERSION}"
                 export PROCS=$(nproc --all)
 
+# Set Indian timezone
+date #show me first
+currentzone=$(date +"%Z")
+oldzone=$currentzone
+echo "Current timezone is $currentzone" && echo ""
+sudo rm -rf /etc/localtime
+sudo ln -s /usr/share/zoneinfo/Asia/Calcutta /etc/localtime
+date #show me first
+currentzone=$(date +"%Z")
+echo "Current timezone is $currentzone" && echo ""
+echo "Timezone changed from $oldzone to $currentzone" && echo ""
+
 # Set Date 
 DATE=$(TZ=Asia/Kolkata date +"%Y%m%d-%T")
 START=$(date +"%s")
