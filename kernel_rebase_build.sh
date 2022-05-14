4451cb3c52fbf173961037e9934c06d851e1436c
Mayur <ultramayur123@gmail.com>
2022-05-14 10:26:33 +0530

    uprev
    Signed-off-by: Mayur <ultramayur123@gmail.com>

--- kernel_rebase_build.sh
+++ kernel_rebase_build.sh
@@ -23,7 +23,7 @@ DEFCONFIG=atoll_defconfig
 KERNEL_DIR=$(pwd)
 
 # The version code of the Kernel
-VERSION=v0.69+3
+VERSION=v0.69+4
 
 # Path of final Image 
 IMAGE=$(pwd)/out/arch/arm64/boot/Image.gz-dtb
