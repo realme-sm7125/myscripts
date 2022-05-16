416d716c63571b919afa6a0bf8e0cdd1c4af4583
Mayur <ultramayur123@gmail.com>
2022-05-16 06:17:52 +0530

    add trb clang
    Signed-off-by: Mayur <ultramayur123@gmail.com>

--- kernel_rebase_build.sh
+++ kernel_rebase_build.sh
@@ -43,8 +43,8 @@ M9=$(pwd)/out/net/bridge/br_netfilter.ko
 M10=$(pwd)/out/drivers/net/wireless/ath/wil6210/wil6210.ko
 M11=$(pwd)/out/drivers/platform/msm/msm_11ad/msm_11ad_proxy.ko
 
-# Compiler which needs to be used (Available for now is proton, azure, atomx, neutron and gcc)
-COMPILER=neutron
+# Compiler which needs to be used (Available for now is proton, azure, atomx, neutron, trb and gcc)
+COMPILER=trb
 
 # Verbose build
 # 0 is Quiet | 1 is verbose | 2 gives reason for rebuilding targets
@@ -112,6 +112,11 @@ clone() {
                 COMPILER=clang
                 echo "|| Cloning Neutron Clang ||"
                 git clone --depth=1 https://gitlab.com/dakkshesh07/neutron-clang clang
+        elif [ $COMPILER = "trb" ]
+        then
+                COMPILER=clang
+                echo "|| Cloning TheRagingBeast Clang ||"
+                git clone --depth=1 https://gitlab.com/varunhardgamer/trb_clang clang
 	fi
 
          echo "|| Cloning Anykernel ||"
