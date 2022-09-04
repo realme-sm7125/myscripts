a1f8c381c7e975edf5544a7e9e1c589cfda8b85e
Mayur <ultramayur123@gmail.com>
2022-09-04 19:15:02 +0530

    prepare for release of basic kernel
    Signed-off-by: Mayur <ultramayur123@gmail.com>

--- kernel_build.sh
+++ kernel_build.sh
@@ -23,7 +23,7 @@ DEFCONFIG=atoll_defconfig
 KERNEL_DIR=$(pwd)
 
 # The version code of the Kernel
-VERSION=v0.69+13
+#VERSION=v0.69
 
 # Path of final Image 
 IMAGE=$(pwd)/out/arch/arm64/boot/Image.gz-dtb
@@ -44,7 +44,7 @@ M10=$(pwd)/out/drivers/net/wireless/ath/wil6210/wil6210.ko
 M11=$(pwd)/out/drivers/platform/msm/msm_11ad/msm_11ad_proxy.ko
 
 # Compiler which needs to be used (Available for now is proton, azure, atomx, neutron and gcc)
-COMPILER=atomx
+COMPILER=proton
 
 # Verbose build
 # 0 is Quiet | 1 is verbose | 2 gives reason for rebuilding targets
@@ -121,7 +121,7 @@ clone() {
 # Export
 export ARCH=arm64
 export SUBARCH=arm64
-export LOCALVERSION="-${VERSION}"
+#export LOCALVERSION="-${VERSION}"
 export KBUILD_BUILD_HOST=mayur
 export KBUILD_BUILD_USER="ultra"
 
@@ -222,7 +222,7 @@ function compile() {
 # Zipping
 function zipping() {
     cd AnyKernel3 || exit 1
-    zip -r9 ${KERNELNAME}-${VERSION}_${MODEL}-${DATE_POSTFIX}.zip *
+    zip -r9 ${KERNELNAME}_${MODEL}-${DATE_POSTFIX}.zip *
     cd ..
 }
 clone
