e476ba52a4d1f24d43ff46e7435f33d02aac7750
Mayur <ultramayur123@gmail.com>
2023-05-17 13:14:05 +0000

    update kernelsu script for update ksu version and pick a commit   ksuversion calculation and logic is based based on the actual    calculation to that of KernelSU
    Signed-off-by: Mayur <ultramayur123@gmail.com>

--- kernelsu.sh
+++ kernelsu.sh
@@ -20,6 +20,10 @@ fi
 
 # clone
 git clone ${repo} ${repodir}
+cd ${repodir}
+git rev-list --count HEAD | tee ksuversion.txt
+cd $DIR
+ksuversion=$(expr 10000 + $(cat ${repodir}/ksuversion.txt) + 200)
 #wget https://git.zx2c4.com/wireguard-linux-compat/snapshot/wireguard-linux-compat-"${ver}".zip
 #unzip wireguard-linux-compat-"${ver}".zip -d wireguard
 
@@ -37,6 +41,30 @@ git commit -s -m "kernelsu: sync with repo
   using script from https://github.com/realme-sm7125/myscripts/blob/main/kernelsu.sh
  Referenced from https://kernelsu.org/guide/how-to-integrate-for-non-gki.html#integrate-with-kprobe
  and edited a bit to be compatible for CI builds"
+echo ""
+
+# add kernelsu nongki amd non kprobe related commit
+echo -ne "Do you want to reeapply commit in kernelsu [y/n]: "
+read -r kksu
+if [[ "${kksu}" == "y" ]]; then
+    echo "Picking" && echo ""
+    git fetch https://github.com/jandaX/android_kernel_xiaomi_joyeuse Tanya
+    git cherry-pick -s 34fc1ce
+    echo "Done"
+elif [[ "${kksu}" == "n" ]]; then
+    echo "Skipping" && echo ""
+fi
+echo ""
+
+# update kernelsu version
+echo "Updating KSU version"
+echo ""
+sed -i "s/^#define KERNEL_SU_VERSION.*/#define KERNEL_SU_VERSION ($ksuversion)/g" drivers/kernelsu/ksu.h
+echo "Updated"
+git add drivers/kernelsu/*
+git commit -s -m "kernelsu: hardcode update kernelsu version
+  using script from https://github.com/realme-sm7125/myscripts/blob/main/kernelsu.sh"
+echo ""
 
 # finalize
 cd "${DIR}" || exit 1
